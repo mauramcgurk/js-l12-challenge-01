1cadc087e6588aca02d4865cb0b2ca8413d8f5f4
Maura McGurk <maura@mauramcgurk.com>
2022-10-02 17:52:02 -0700

    Attempt to randomly select index of image

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function () {
-    const res = await fetch (
-        "https://picsum.photos/v2/list?limit=100");
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 };
-getImage();
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+};
